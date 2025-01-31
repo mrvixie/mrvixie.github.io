9c204e0cd22e132f5b55a23059ffa14365ec2c46
VIXIE <141617502+mrvixie@users.noreply.github.com>
2025-01-31 23:32:48 +0300

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,53 +1,33 @@
-/*
-THIS CODE BY zoliryzik
-*/
-const boxProducts = document.getElementById("news");
-/*
-ЗАПОЛНИТЕЛИ УСЛУГ 
-*/
+// Получаем шаблон модального окна
+var modalTemplate = document.getElementById("modalTemplate");
 
-const p1 = [
-    {
-        "TITLE": "ПК диагностика",
-        "LORE": "Компьютерная диагностика автомобиля, адаптация дросселя и др.",
-        "SUM": "от 500 ₽",
-        "IMG": "img/products/1.jpg",
-        "TEXTBTN": "Подробнее",
-        "URLBTN": "./index.html",
-        "IDBTN": "myBtn",  
-    }
-]
-const owner = [
-    {
-        "name": "ZoLiryzik and MrVixie",
-        "URL": "https://zoliryzik.github.io",
-        "SUM": "от 500 ₽",
-        "IMG": "img/products/pustota.png",
-        "TEXTBTN": "Подробнее",
-        "URLBTN": "./index.html",
-    }
-]
-/*
-Добавление баннеров 
-*/
-for (let indexOne = 0; indexOne < p1.length; indexOne++) {
-/* 
-Переменные зависят от Кол-ва баннеров
-*/
-    const elementProduct = p1[indexOne];
+// Получаем кнопку, которая открывает модальное окно
+var openModalBtn = document.getElementById("openModalBtn");
 
+// Получаем элемент <span>, который закрывает модальное окно
+var closeBtn = modalTemplate.getElementsByClassName("close")[0];
 
-    
-    const elementowner = owner[indexOne];   
-/*
-Вывод в консоли
-*/
-    console.log('Site by '+elementowner.name +' for ExpressAuto\nЛичный сайт: ' + elementowner.URL );
-/*
-ВЫВОД УСЛУГ НЕ ТРОГАТЬ 
-*/  
-            const p11 = "    <div class='container'><div class='card'><figure class='card__poster'><img class='card-poster__image' src='"+elementProduct.IMG+"?w=800&amp;h=500&amp;fit=crop' alt='poster'></figure><div class='card__databox'><h3 class='card-databox__heading'>"+ elementProduct.TITLE +"</h3><div class='card-databox__description'>"+ elementProduct.LORE +"</div><div class='card-databox__description'><h1>"+elementProduct.SUM+"</h1></div><button class='button' id="+elementProduct.IDBTN+">"+ elementProduct.TEXTBTN +"</button></div></div>"
-            
+// Когда пользователь нажимает на кнопку, открывается модальное окно с динамическим содержимым
+openModalBtn.onclick = function() {
+  var title = this.getAttribute("data-title");
+  var imgSrc = this.getAttribute("data-img");
+  var description = this.getAttribute("data-description");
 
-            boxProducts.innerHTML += p11;
+  modalTemplate.getElementsByClassName("modal-title")[0].innerText = title;
+  modalTemplate.getElementsByClassName("modal-img")[0].src = imgSrc;
+  modalTemplate.getElementsByClassName("modal-description")[0].innerText = description;
+
+  modalTemplate.style.display = "block";
+}
+
+// Когда пользователь нажмет на <span> (x), закрывается модальное окно
+closeBtn.onclick = function() {
+  modalTemplate.style.display = "none";
+}
+
+// Когда пользователь нажимает в любом месте за пределами модального окна, закройте его
+window.onclick = function(event) {
+  if (event.target == modalTemplate) {
+    modalTemplate.style.display = "none";
+  }
 }
