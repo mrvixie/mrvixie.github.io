13d75336527f41a7b6b9b960ee9d03f416cc8e2c
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2025-03-17 17:38:05 +0300

    Update script.js

--- q/script.js
+++ q/script.js
@@ -1,5 +1,5 @@
 // Подключение blocks.js
-import { blocks } from './blocks.js';
+import { blocks } from 'https://mrvixie.github.io/q/blocks.js';
 
 // Ваш существующий код
 const container = document.getElementById('container');
