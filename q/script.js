c0ddd801741b1fab10d93dd5747813af09f12b8a
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2025-03-17 17:40:18 +0300

    Update script.js

--- q/script.js
+++ q/script.js
@@ -1,16 +1,43 @@
-// Подключение blocks.js
-import { blocks } from 'https://mrvixie.github.io/q/blocks.js';
+const blocks = [
+    {
+        text: `{# Код для внесения денег в банк #}
+{% set money = member.getAttribute('money') %}
+{% require arguments.get(1) returning 'Использование: !положить <кол-во>' %}
+{% require number(arguments.get(1)) returning 'Это не число' %}
+{% require arguments.get(1) > 0 returning '**Вы не можете положить отрицательную сумму в банк**' %}
+{% set abank = arguments.get(1) %}
+{% set mbank = member.getAttribute('money').value %}
+{% if mbank < abank %}
+{% return '**У вас недостаточно денег на балансе**' %}
+{% else %}
+{% if mbank >= abank %}
+{% do member.getAttribute('money').decrement(abank) %}
+{% do member.getAttribute('bank').increment(abank) %}
+**Вы успешно положили: *{{abank}}* монет в банк**
+{% endif %}{% endif %}`,
+        extra: "Данные о транзакциях и банке",
+        label: "Блок 1 - Нажмите кнопку, чтобы копировать или просмотреть"
+    },
+    {
+        text: "Пример текста для копирования из Блока 2",
+        extra: "Второстепенные данные блока 2",
+        label: "Блок 2 - Нажмите кнопку, чтобы копировать или просмотреть"
+    },
+    {
+        text: "Другой пример текста для копирования из Блока 3",
+        extra: "Скрытые данные блока 3",
+        label: "Блок 3 - Нажмите кнопку, чтобы копировать или просмотреть"
+    }
+];
 
-// Ваш существующий код
 const container = document.getElementById('container');
 
 // Создание блоков
 blocks.forEach(block => {
     const div = document.createElement('div');
     div.className = 'block';
-    div.setAttribute('data-text', block.code || block.text);
+    div.setAttribute('data-text', block.text);
     div.setAttribute('data-extra', block.extra);
-    div.setAttribute('data-title', block.name);
     div.innerHTML = `
         ${block.label}
         <button class="copy-button">Копировать</button>
@@ -18,9 +45,6 @@ blocks.forEach(block => {
     `;
     container.appendChild(div);
 });
-
-// Остальной код взаимодействия
-
 
 document.querySelectorAll('.copy-button').forEach(button => {
     button.addEventListener('click', function() {
