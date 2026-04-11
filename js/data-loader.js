9be9b6d3f6a74810432840781410f887c6ee387e
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2026-04-12 01:01:34 +0300

    Update data-loader.js

--- js/data-loader.js
+++ js/data-loader.js
@@ -48,10 +48,10 @@ const DataLoader = {
         this.listeners.forEach(cb => cb(this.data));
     },
     
-    getStats() { return this.get('stats', {}); console.log("API BY ZOLIRYZIK")},
-    getProfile() { return this.get('profile', {}); console.log("API BY ZOLIRYZIK")},
-    getServices() { return this.get('services', []); console.log("API BY ZOLIRYZIK")},
-    getPortfolio() { return this.get('portfolio', []); console.log("API BY ZOLIRYZIK")},
+    getStats() { return this.get('stats', {});},
+    getProfile() { return this.get('profile', {});},
+    getServices() { return this.get('services', []);},
+    getPortfolio() { return this.get('portfolio', []); },
     getBlog() { return this.get('blog', []); },
     getKlondike() { return this.get('klondike', []); },
     getJuniper() { return this.get('juniper', []); },
@@ -82,3 +82,5 @@ const DataLoader = {
 };
 
 window.DataLoader = DataLoader;
+
+console.log("API BY ZOLIRYZIK")
