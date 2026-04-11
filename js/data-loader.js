78da135860a6dab4912f538d6a578505b548c977
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2026-04-12 00:28:51 +0300

    Update data-loader.js

--- js/data-loader.js
+++ js/data-loader.js
@@ -1,6 +1,6 @@
 const DataLoader = {
     data: null,
-    apiUrl: 'https://srv.zoliryzik.ru/api/content',
+    apiUrl: 'https://api.mr-vixie.su/api/content',
     refreshInterval: 300000,
     lastLoad: 0,
     listeners: [],
